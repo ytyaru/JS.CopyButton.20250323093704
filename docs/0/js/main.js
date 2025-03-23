e4caad106106a0e931ad96b1c468e234d33bc25d
ytyaru <yttry0@gmail.com>
2025-03-23 18:18:08 +0900

    Created a repository

---
+++ docs/0/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.CopyButton.20250323093704/`}, 'CopyButton')),
+        van.tags.p('クリップボードにコピーするボタン（CustomElement）。'),
+//        van.tags.p('A button to copy to clipboard (CustomElement).'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
