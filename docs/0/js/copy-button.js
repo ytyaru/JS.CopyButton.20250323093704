64d4cd74be0e8b183cb40f1b8445bbffd950d533
ytyaru <yttry0@gmail.com>
2025-03-24 11:12:32 +0900

    add 0

--- docs/0/js/copy-button.js
+++ docs/0/js/copy-button.js
@@ -4,6 +4,8 @@ class CopyButtonElement extends HTMLElement {
         super();
         this._options = {...this.defaultOptions, ...options};
         this._shadow = this.attachShadow({mode:'open'});
+        this.for = null;
+        this.value = `Set the text to be copied in the 'value' attribute, or set the CSS selector of the target element in the 'for' attribute.`;
     }
     connectedCallback() {
         console.log("カスタム要素がページに追加されました。");
@@ -12,77 +14,33 @@ class CopyButtonElement extends HTMLElement {
         console.log(this.textContent)
         setTimeout(()=>{
             console.log('setTimeout:', this.textContent);
-            console.log('setTimeout:', this.innerHTML);
-            this.#makeDefaultBaseEl();
             this.#makeEl();
-            this.#addEvent();
-            console.log('setTimeout:', this.children);
         }, 0);
     }
     disconnectedCallback() {console.log("カスタム要素がページから除去されました。");}
     adoptedCallback() {console.log("カスタム要素が新しいページへ移動されました。");}
     attributeChangedCallback(name, oldValue, newValue) {
         console.log(`属性 ${name} が変更されました。`);
+        if ('value'===name) {this.value = newValue}
+        else if ('for'===name) {this.for = newValue}
     }
-    static get observedAttributes() {return [''];}
-
-    get el() {return this._shadow.querySelector(`div[name="overlay-box"]`)}
-    get baseEl() {return this._shadow.querySelector(`.base-el`)}
-    get defaultOptions() { return {
-        baseEl: null,
-    } }
-    #makeStyle() {
-        const style = document.createElement('style');
-        style.textContent = ``;
-        return style;
-    }
-    #makeDefaultBaseEl() {
-        const innerEl = [...this.children].find(el=>'base'===el.getAttribute('name'));
-        if (innerEl) {this._options.baseEl = innerEl}
-        if (this._options.baseEl instanceof HTMLElement) {
-            if (!this._options.baseEl.classList.contains('base-el')) {
-                this._options.baseEl.classList.add('base-el');
-            }
-        } else {
-            const el = document.createElement('textarea');
-            el.classList.add('base-el');
-            this._options.baseEl = el;
-        }
-    }
+    static get observedAttributes() {return ['value', 'for'];}
+    get value() {return this._value}
+    set value(v) {this._value = v;}
+    get el() {return this._shadow.querySelector(`:host button`)}
     #makeEl() {
         const button = document.createElement('button');
-        button.textContent = '📋';
+        if (this.innerHTML) {button.innerHTML = this.innerHTML;}
+        else {button.textContent = '📋';}
         button.addEventListener('click', async(e)=>{
-            await Clipboard.copy()
+            const el = document.querySelector(this.for);
+            if (el) {
+                for (let attr of ['value', 'textContent', 'innerHTML']) {
+                    if (attr in el) {await Clipboard.write(el[attr])}
+                }
+            } else {await Clipboard.write(this.value)}
         })
         this._shadow.append(button);
-    }
-    #makeArea() {
-        const area = document.createElement('div');
-        area.setAttribute('name', 'overlay-area');
-        area.classList.add('overlay-area');
-        const NAMES = 'top-left top top-right left center right bottom-left bottom bottom-right'.split(' ');
-        // 対象要素のみ取得する。余計な要素は消す。
-        for (let node of this.childNodes) {
-            if (Node.ELEMENT_NODE===node.nodeType) {
-                if (NAMES.includes(node.getAttribute('name'))) {
-                    node.classList.add('child');
-                    area.append(node)
-                } else {node.remove()}
-            }
-        }
-        return area;
-    }
-    #addEvent() {
-        (new ResizeObserverX((entries,observer)=>{
-            const E = entries[0].target.style;
-            const B = entries[0].border[0];
-            const T = this.el.style;
-            E.contentVisibility = 'hidden';
-            T.width = B.width;
-            T.height = B.height;
-            E.contentVisibility = 'visible';
-        })).observe(this.baseEl);
     }
 }
 customElements.define('copy-button', CopyButtonElement);
